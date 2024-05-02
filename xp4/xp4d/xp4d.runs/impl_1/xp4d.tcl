65a4bb290cfadfe85119e165cf06b016d8fcf2a1
Trilochan Kumar <126813766+trilochan-kumar@users.noreply.github.com>
2024-05-02 16:20:46 +0530

    Better version Tried.
    The better version is working very well.

--- xp4/xp4d/xp4d.runs/impl_1/xp4d.tcl
+++ xp4/xp4d/xp4d.runs/impl_1/xp4d.tcl
@@ -127,7 +127,7 @@ set rc [catch {
   create_msg_db write_bitstream.pb
   set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 3
-  set_param synth.incrementalSynthesisCache C:/Users/Thrillu/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-19720-Lisa/incrSyn
+  set_param synth.incrementalSynthesisCache D:/ATK/Programming/FPGA_5204/FPGA-5204-Lab-XPs/xp4/xp4d/.Xil/Vivado-19892-Lisa/incrSyn
   set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 6  }
   open_checkpoint xp4d_routed.dcp
--- xp4/xp4d/xp4d.srcs/constrs_1/new/xp4d.xdc
+++ xp4/xp4d/xp4d.srcs/constrs_1/new/xp4d.xdc
@@ -5,4 +5,5 @@ set_property IOSTANDARD LVCMOS33 [get_ports t]
 set_property PACKAGE_PIN U18 [get_ports clk]
 set_property PACKAGE_PIN T17 [get_ports q]
 set_property PACKAGE_PIN R18 [get_ports qb]
-set_property PACKAGE_PIN W15 [get_ports t]
+set_property PACKAGE_PIN V15 [get_ports t]
+
